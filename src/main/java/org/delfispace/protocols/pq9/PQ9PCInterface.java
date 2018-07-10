a30e130284380bb8fe42d5c40de553eb3f179943
Stefano Speretta <s.speretta@tudelft.nl>
2018-07-10 22:25:34 +0200

    Add a new interpreter for the serial port connection with unit test

---
+++ src/main/java/org/delfispace/protocols/pq9/PQ9PCInterface.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (C) 2018 , Nikitas Chronas, Stefano Speretta
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.delfispace.protocols.pq9;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ *
+ * @author Nikitas Chronas <N.ChronasFoteinakis@tudelft.nl>
+ * @author Stefano Speretta <s.speretta@tudelft.nl>
+ */
+public class PQ9PCInterface 
+{
+    private final InputStream in;
+    private final OutputStream out;
+    private PQ9Receiver callback;
+    private readerThread reader;
+    private final ByteArrayOutputStream bs = new ByteArrayOutputStream();
+    private boolean startFound = false;
+    private boolean controlFound = false;
+    private int sizeFrame = -1;
+
+    static final int HLDLC_START_FLAG = 0x7E;
+    static final int HLDLC_CONTROL_FLAG = 0x7D;
+    static final int HLDLC_STOP_FLAG = 0x7C;
+    static final int HLDLC_ESCAPE_START_FLAG = 0x5E;
+    static final int HLDLC_ESCAPE_CONTROL_FLAG = 0x5D;
+    static final int HLDLC_ESCAPE_STOP_FLAG = 0x5C;
+
+    public PQ9PCInterface(InputStream in, OutputStream out) 
+    {
+        this.in = in;
+        this.out = out;
+    }
+
+    public void setReceiverCallback(PQ9Receiver clb) 
+    {
+        if (callback != null) 
+        {            
+            reader.running = false;
+        }
+
+        callback = clb;
+
+        if (callback != null) 
+        {
+            reader = new readerThread();
+            reader.start();
+        }
+    }
+
+    public PQ9 read() throws IOException 
+    {
+        if (callback == null) 
+        {
+            return blockingread();
+        }
+        return null;
+    }
+
+    private PQ9 blockingread() throws IOException 
+    {
+        int tmprx = in.read();
+
+        while (tmprx != -1) 
+        {
+            byte rx = (byte) (tmprx & 0xFF);
+            if (rx == HLDLC_START_FLAG) 
+            {
+                // clear the buffer and get ready to process a new frame
+                bs.reset();
+                startFound = true;
+                controlFound = false;
+                sizeFrame = -1;
+            } 
+            else if (startFound) 
+            {
+                // only process data if a flag was found            
+                if (rx == HLDLC_CONTROL_FLAG) 
+                {
+                    controlFound = true;
+                } 
+                else if (controlFound) 
+                {
+                    switch (rx) 
+                    {
+                        case HLDLC_ESCAPE_START_FLAG:
+                            bs.write(HLDLC_START_FLAG);
+                            break;
+                        case HLDLC_ESCAPE_CONTROL_FLAG:
+                            bs.write(HLDLC_CONTROL_FLAG);
+                            break;
+                        case HLDLC_ESCAPE_STOP_FLAG:
+                            bs.write(HLDLC_STOP_FLAG);
+                            break;
+                        default:
+                            // illegal sequence, aborting
+                            startFound = false;
+                            controlFound = false;
+                            // throw exception here
+                            System.out.println("exception 1");
+                            break;
+                    }
+                    controlFound = false;
+                } 
+                else 
+                {
+                    // new data byte, add it to the buffer
+                    bs.write(rx);
+                    if ((sizeFrame != -1) && (sizeFrame == bs.size() - 5))
+                    {
+                        startFound = false;
+                        controlFound = false;
+                        sizeFrame = -1;
+                        try 
+                        {
+                            return new PQ9(bs.toByteArray());                            
+                        } catch (PQ9Exception ex) 
+                        {
+                            // the frame is not valid, throw away the data and wait for a new frame
+                            return null;
+                        }                        
+                    }
+                }
+            }
+            // initialize the counter to catch the frame size byte and use it to 
+            // detect frame termination
+            if (bs.size() == 2)
+            {
+                sizeFrame = rx & 0xFF;
+            }
+            
+            // prevent the buffer from growing too much
+            if (bs.size() > 256)
+            {
+                bs.reset();
+                startFound = false;
+                controlFound = false;
+                sizeFrame = -1;
+            }
+            // read new byte
+            tmprx = in.read();
+        }
+
+        // no full frame received yet
+        return null;
+    }
+
+    public synchronized void send(PQ9 frame) throws IOException 
+    {
+        out.write(HLDLC_START_FLAG);
+        byte[] data = frame.getFrame();
+        for (int i = 0; i < data.length; i++) 
+        {
+            byte tx = data[i];
+            switch (tx) 
+            {
+                case HLDLC_START_FLAG:
+                    out.write(HLDLC_CONTROL_FLAG);
+                    out.write(HLDLC_ESCAPE_START_FLAG);
+                    break;
+                case HLDLC_CONTROL_FLAG:
+                    out.write(HLDLC_CONTROL_FLAG);
+                    out.write(HLDLC_ESCAPE_CONTROL_FLAG);
+                    break;
+                case HLDLC_STOP_FLAG:
+                    out.write(HLDLC_CONTROL_FLAG);
+                    out.write(HLDLC_ESCAPE_STOP_FLAG);
+                    break;
+                default:
+                    out.write(tx);
+                    break;
+            }
+        }
+        out.write(HLDLC_STOP_FLAG);
+        out.flush();
+    }
+
+    class readerThread extends Thread 
+    {
+        public boolean running;
+
+        @Override
+        public void run() 
+        {
+            running = true;
+            try 
+            {
+                while (running) 
+                {
+                    PQ9 d = blockingread();
+                    if (d != null) 
+                    {
+                        callback.received(d);
+                    }
+                }
+            } catch (IOException ex) 
+            {
+                running = false;
+            }
+        }
+    }
+}
---
+++ src/test/java/org/delfispace/protocols/pq9/PQ9PCInterfaceTest.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (C) 2018 , Stefano Speretta
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.delfispace.protocols.pq9;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.delfispace.protocols.LoopbackStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ *
+ * @author Stefano Speretta <s.speretta@tudelft.nl>
+ */
+public class PQ9PCInterfaceTest 
+{
+    private PQ9PCInterface protocol;
+    private int receivedFrames = 0;
+            
+    @BeforeClass
+    public static void setUpClass() 
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() 
+    {
+    }
+    
+    @Before
+    public void setUp() 
+    {
+        
+    }
+    
+    @After
+    public void tearDown() 
+    {
+    }
+
+    private void runTest(byte[] input, byte[] output) throws IOException
+    {
+        ByteArrayInputStream is = new ByteArrayInputStream(input);
+        protocol = new PQ9PCInterface(is, null);
+  
+        byte[] data = protocol.read().getFrame();
+        for (int i = 0; i < data.length; i++)
+        {
+            System.out.print(String.format("%02X ", data[i]));
+        }        
+        System.out.println();
+
+        Assert.assertArrayEquals("Error", output, data);
+    }
+
+    @Test
+    public void testBlockingReceive() throws IOException 
+    {
+        final String methodName =
+            Thread.currentThread().getStackTrace()[1].getMethodName();
+
+        System.out.println( "Test Case: " + methodName + "()" );
+        PQ9 t;
+        try {
+            t = new PQ9(1, 2, new byte[]{(byte)0x7E, (byte)0x7D, (byte)0x7C});
+            System.out.println(t);
+        } catch (PQ9Exception ex) {
+            Logger.getLogger(PQ9PCInterfaceTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        runTest(new byte[]{(byte)0x7E, (byte)0x01, (byte)0x00, (byte)0x02, (byte)0xEE, (byte)0xDB}, 
+                new byte[]{(byte)0x01, (byte)0x00, (byte)0x02, (byte)0xEE, (byte)0xDB});
+        runTest(new byte[]{(byte)0x7E, (byte)0x01, (byte)0x01, (byte)0x02, (byte)0x01, (byte)0x07, (byte)0xB3}, 
+                new byte[]{(byte)0x01, (byte)0x01, (byte)0x02, (byte)0x01, (byte)0x07, (byte)0xB3});        
+        runTest(new byte[]{(byte)0x7E, (byte)0x01, (byte)0x03, (byte)0x02, (byte)0x7D, (byte)0x5E, (byte)0x7D, (byte)0x5D, (byte)0x7D, (byte)0x5C, (byte)0x1D, (byte)0x4A}, 
+                new byte[]{(byte)0x01, (byte)0x03, (byte)0x02, (byte)0x7E, (byte)0x7D, (byte)0x7C, (byte)0x1D, (byte)0x4A});      
+    }
+
+    @Test
+    public void testPartial() throws IOException
+    {
+        final String methodName =
+            Thread.currentThread().getStackTrace()[1].getMethodName();
+
+        System.out.println( "Test Case: " + methodName + "()" );
+        
+        byte[] input = new byte[]{(byte)0x7E, (byte)0x01, (byte)0x01, (byte)0x02, (byte)0x01, (byte)0x07, (byte)0xB3, (byte)0xFF, (byte)0x7E, (byte)0x01, (byte)0x00, (byte)0x02, (byte)0xEE, (byte)0xDB};
+        
+        ByteArrayInputStream is = new ByteArrayInputStream(input);
+        protocol = new PQ9PCInterface(is, null);
+  
+        PQ9 f = protocol.read();
+    
+        System.out.println(f);
+        Assert.assertArrayEquals("Error", f.getFrame(), new byte[] {(byte)0x01, (byte)0x01, (byte)0x02, (byte)0x01, (byte)0x07, (byte)0xB3});
+        
+        f = protocol.read();       
+        System.out.println(f);
+        Assert.assertArrayEquals("Error", f.getFrame(), new byte[] {(byte)0x01, (byte)0x00, (byte)0x02, (byte)0xEE, (byte)0xDB});
+    }
+
+    @Test
+    public void testSend() throws IOException, PQ9Exception
+    {
+        final String methodName =
+            Thread.currentThread().getStackTrace()[1].getMethodName();
+
+        System.out.println( "Test Case: " + methodName + "()" );
+        
+        PQ9 frame = new PQ9(1, 2, new byte[]{(byte)0x7E, (byte)0x7D, (byte)0x7C});            
+        
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        LoopbackStream ls = new LoopbackStream();
+        
+        protocol = new PQ9PCInterface(ls.getInputStream(), os);
+        byte[] output = new byte[]{(byte)0x7E, (byte)0x01, (byte)0x03, (byte)0x02, (byte)0x7D, (byte)0x5E, (byte)0x7D, (byte)0x5D, (byte)0x7D, (byte)0x5C, (byte)0x1D, (byte)0x4A, (byte)0x7C, (byte)0x7E, (byte)0x01, (byte)0x03, (byte)0x02, (byte)0x7D, (byte)0x5E, (byte)0x7D, (byte)0x5D, (byte)0x7D, (byte)0x5C, (byte)0x1D, (byte)0x4A, (byte)0x7C};
+  
+        protocol.send(frame);
+        protocol.send(frame);
+        
+        byte[] data = os.toByteArray();
+        
+        for (int i = 0; i < data.length; i++)
+        {
+            System.out.print(String.format("%02X ", data[i]));
+        }        
+        System.out.println();        
+        
+        Assert.assertArrayEquals("Error", data, output);
+    }
+
+    @Test
+    public void testAsynchReceive() throws IOException, InterruptedException, PQ9Exception
+    {
+        final String methodName =
+            Thread.currentThread().getStackTrace()[1].getMethodName();
+
+        System.out.println( "Test Case: " + methodName + "()" );
+        
+        PQ9 frame = new PQ9(1, 2, new byte[]{(byte)0x7E, (byte)0x7D, (byte)0x7C});
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        LoopbackStream ls = new LoopbackStream();
+        
+        protocol = new PQ9PCInterface(ls.getInputStream(), ls.getOutputStream());
+        byte[] input = new byte[]{(byte)0x01, (byte)0x02, (byte)0x7E, (byte)0xFF, (byte)0xFE };
+               
+        receivedFrames = 0;
+        protocol.setReceiverCallback((PQ9 data) -> {
+            Assert.assertArrayEquals("Error", data.getFrame(), frame.getFrame());
+            receivedFrames++;
+        });
+        
+        protocol.send(frame);
+        protocol.send(frame);
+        
+        ls.join();
+        Assert.assertEquals("Error", receivedFrames, 2);
+    }
+}
