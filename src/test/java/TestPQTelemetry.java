e67f58bad8db0545710cce4c11cf56f8ba408c0e
Stefano Speretta <s.speretta@tudelft.nl>
2019-09-13 16:40:02 +0200

    Fixed telemetry parser unit test
    Test fields have been removed in a previous commit

--- src/test/java/TestPQTelemetry.java
+++ src/test/java/TestPQTelemetry.java
@@ -1,21 +1,13 @@
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.delfispace.pq9debugger.Configuration;
-import org.delfispace.pq9debugger.Main;
 import org.delfispace.protocols.pq9.PQ9;
 import org.delfispace.protocols.pq9.PQ9Exception;
-import org.json.simple.parser.ParseException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.xtce.toolkit.XTCEContainerContentEntry;
 import org.xtce.toolkit.XTCEContainerContentModel;
 import org.xtce.toolkit.XTCEDatabase;
 import org.xtce.toolkit.XTCEDatabaseException;
@@ -76,15 +68,13 @@ public class TestPQTelemetry
 
         System.out.println( "Test Case: " + methodName + "()" );
         
-        PQ9 pingRequest = new PQ9(1, 1, new byte[]{
-            (byte)0x00, (byte)0x00, (byte)0x11, (byte)0x01});
+        PQ9 pingRequest = new PQ9(1, 1, new byte[]{(byte)0x11, (byte)0x01});
 
         XTCEContainerContentModel pingRequestDecoded = 
                 stream.processStream( pingRequest.getFrame() );
         Assert.assertEquals("PingRequest", pingRequestDecoded.getName());
         
-        PQ9 pingResponse = new PQ9(1, 1, new byte[]{
-            (byte)0x00, (byte)0x00, (byte)0x11, (byte)0x02});
+        PQ9 pingResponse = new PQ9(1, 1, new byte[]{(byte)0x11, (byte)0x02});
         
         XTCEContainerContentModel pingResponseDecoded = 
                 stream.processStream( pingResponse.getFrame() );
@@ -100,7 +90,7 @@ public class TestPQTelemetry
         System.out.println( "Test Case: " + methodName + "()" );
         
         PQ9 OBCHKRequest = new PQ9(1, 1, 
-                new byte[]{(byte)0x00, (byte)0x00, (byte)0x03, (byte)0x01});
+                new byte[]{(byte)0x03, (byte)0x01});
         
         XTCEContainerContentModel OBCHKRequestDecoded = 
                 stream.processStream( OBCHKRequest.getFrame() );
@@ -108,10 +98,10 @@ public class TestPQTelemetry
         // check if it is actually coming from OBC
                 
         PQ9 OBCHKResponse = new PQ9(1, 1, new byte[]{ 
-            (byte)0x0A, (byte)0xCD, (byte)0x03, (byte)0x02, (byte)0x00, (byte)0x54, 
-            (byte)0x1E, (byte)0xD7, (byte)0xDE, (byte)0xAD, (byte)0xBE, (byte)0xEF, 
-            (byte)0x01, (byte)0xA1, (byte)0x00, (byte)0x03, (byte)0x00, (byte)0x1F, 
-            (byte)0x0A, (byte)0x36, (byte)0xCA, (byte)0xFE});
+            (byte)0x03, (byte)0x02, (byte)0x00, (byte)0x54, (byte)0x1E, (byte)0xD7, 
+            (byte)0xDE, (byte)0xAD, (byte)0xBE, (byte)0xEF, (byte)0x01, (byte)0xA1, 
+            (byte)0x00, (byte)0x03, (byte)0x00, (byte)0x1F, (byte)0x0A, (byte)0x36, 
+            (byte)0xCA, (byte)0xFE});
             
         XTCEContainerContentModel OBCHKResponseDecoded = 
                 stream.processStream( OBCHKResponse.getFrame() );
@@ -127,7 +117,7 @@ public class TestPQTelemetry
         System.out.println( "Test Case: " + methodName + "()" );
         
         PQ9 EPSHKRequest = new PQ9(2, 1, 
-                new byte[]{(byte)0x00, (byte)0x00, (byte)0x03, (byte)0x01});
+                new byte[]{(byte)0x03, (byte)0x01});
         
         XTCEContainerContentModel OBCHKRequestDecoded = 
                 stream.processStream( EPSHKRequest.getFrame() );
@@ -135,19 +125,19 @@ public class TestPQTelemetry
         // check if it is actually coming from EPS
 
         PQ9 EPSHKResponse = new PQ9(1, 2, new byte[]{ 
-            (byte)0x00, (byte)0x00, (byte)0x03, (byte)0x02, (byte)0x00, (byte)0x14, 
-            (byte)0x89, (byte)0x13, (byte)0xDE, (byte)0xAD, (byte)0xBE, (byte)0xEF, 
-            (byte)0x03, (byte)0x3A, (byte)0x00, (byte)0x83, (byte)0xF0, (byte)0x00, 
+            (byte)0x03, (byte)0x02, (byte)0x00, (byte)0x14, (byte)0x89, (byte)0x13, 
+            (byte)0xDE, (byte)0xAD, (byte)0xBE, (byte)0xEF, (byte)0x03, (byte)0x3A, 
+            (byte)0x00, (byte)0x83, (byte)0xF0, (byte)0x00, (byte)0x00, (byte)0x00, 
             (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, 
             (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, 
-            (byte)0x00, (byte)0x00, (byte)0x00, (byte)0xFF, (byte)0xFF, (byte)0xFF, 
-            (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0x00, 
-            (byte)0xB4, (byte)0x00, (byte)0x93, (byte)0x00, (byte)0x67, (byte)0x00, 
-            (byte)0x84, (byte)0x0A, (byte)0x35, (byte)0x0A, (byte)0x36, (byte)0x0A, 
-            (byte)0x38, (byte)0x0A, (byte)0x36, (byte)0xFF, (byte)0xFF, (byte)0xFF, 
-            (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0x01, (byte)0x43, 
-            (byte)0x00, (byte)0x6E, (byte)0x09, (byte)0x5D, (byte)0x02, (byte)0x95, 
-            (byte)0x0A, (byte)0x39, (byte)0xCA, (byte)0xFE});
+            (byte)0x00, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, 
+            (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0x00, (byte)0xB4, (byte)0x00, 
+            (byte)0x93, (byte)0x00, (byte)0x67, (byte)0x00, (byte)0x84, (byte)0x0A, 
+            (byte)0x35, (byte)0x0A, (byte)0x36, (byte)0x0A, (byte)0x38, (byte)0x0A, 
+            (byte)0x36, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, 
+            (byte)0xFF, (byte)0xFF, (byte)0x01, (byte)0x43, (byte)0x00, (byte)0x6E, 
+            (byte)0x09, (byte)0x5D, (byte)0x02, (byte)0x95, (byte)0x0A, (byte)0x39, 
+            (byte)0xCA, (byte)0xFE});
             
         XTCEContainerContentModel EPSHKResponseDecoded = 
                 stream.processStream( EPSHKResponse.getFrame() );
