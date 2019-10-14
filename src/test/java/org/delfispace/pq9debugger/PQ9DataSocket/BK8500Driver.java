4ae00ba8be24b03fcf52ca98ca6dc585ea59281d
MFvandenBos <35063713+MFvandenBos@users.noreply.github.com>
2019-10-14 13:16:41 +0200

    Updated BK8500 driver - works
    Driver is able to send and recieve data now

--- src/test/java/org/delfispace/pq9debugger/PQ9DataSocket/BK8500Driver.java
+++ src/test/java/org/delfispace/pq9debugger/PQ9DataSocket/BK8500Driver.java
@@ -42,11 +42,24 @@ public class BK8500Driver {
     {        
         comPort = SerialPort.getCommPort(port);               
         comPort.openPort();
-        comPort.setComPortParameters(38400, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+        System.out.println("isOpen: " + comPort.isOpen() + " ");
+        comPort.setComPortParameters(9600, 8, SerialPort.ONE_STOP_BIT, SerialPort.NO_PARITY);
+        int fCs;
         comPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+        //fCs = comPort.getFlowControlSettings();
+        //System.out.println("Flow control Settings: "+ fCs);
+        
+        //comPort.setFlowControl(SerialPort.FLOW_CONTROL_RTS_ENABLED | SerialPort.FLOW_CONTROL_DTR_ENABLED);
+        //comPort.setDTR();
+        
+        //omPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN)
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 2000, 0);
+
         is = comPort.getInputStream();
         os = comPort.getOutputStream();
+        
+        System.out.println("is " + is);
+        System.out.println("os " + os);
     }
     //remember to close the port only one object can use the same port.     
     public void closePort()
@@ -55,20 +68,15 @@ public class BK8500Driver {
         comPort.closePort();
     }
        
-    public int checkSum(int[] cmd)
+    public int checkSum(byte[] cmd)
     {
         // This method calculates the checksum. 
         int sum =0;
         for(int i = 0; i<cmd.length;i++)
         {  
-            System.out.println(cmd[i]);
-            sum = (sum + cmd[i]);
-            System.out.println("csum;  " + sum );
+            sum = (sum + (((int)cmd[i]) & 0xFF));
         }
-        System.out.println("final csum "+ sum);
-        System.out.println("final 0FF&csum  "+ (0xFF&sum) );
-        int extra = 0xFF; // 
-        return (extra&sum);
+        return sum & 0xFF;
     }
     
     public static String byteToString(byte b) 
@@ -90,28 +98,24 @@ public class BK8500Driver {
     public void startRemoteOperation() throws IOException{
         //enable remote mode
         byte[] cmd = new byte[26];
-        int[] cmdp = new int[26];
+
         // set all command bytes to 0
         for(int item : cmd){
             cmd[item] = 0x00;
-            cmdp[item] = 0;
-            System.out.println(byteToString(cmd[item]));
         }
         System.out.println("printing empty command");
         System.out.println(Arrays.toString(cmd));
         cmd[0]=(byte) 0xAA;
-        cmdp[0] = 0xAA;
         cmd[2]=(byte) 0x20;
-        cmdp[2] = 0x20;
         cmd[3]=(byte) 1;
-        cmdp[3] = 1;
-        cmd[25]=(byte)checkSum(cmdp);
+        cmd[25]=(byte)checkSum(cmd);
         // now we are ready to send the command
         System.out.println("Print Command Stream");
         for(int item = 0; item<cmd.length; item++)
         {  
-            System.out.println(byteToString(cmd[item]));
+            System.out.print(String.format("%02X ", cmd[item] & 0xFF));
         }
+        System.out.println();
         sendCommand(cmd);
     }
     
@@ -121,21 +125,54 @@ public class BK8500Driver {
     
     private void sendCommand(byte[] cmd) throws IOException
     {
-        System.out.println("I am sending");
-        os.write(cmd);
-        try 
+         try 
         {
-            Thread.sleep(200);
+            Thread.sleep(10);
         } catch (InterruptedException ex) 
         {
             // ignoring the error
         }
+        System.out.println("I am sending " + os);
+        os.write(cmd);
+        os.flush();
+        try 
+        {
+            Thread.sleep(150);
+        } catch (InterruptedException ex) 
+        {
+            // ignoring the error
+            ex.printStackTrace();
+        }
+        
     }
 
     private byte[] getResponseByte() throws IOException
     {
+        try 
+        {
+            Thread.sleep(10);
+        } catch (InterruptedException ex) 
+        {
+            // ignoring the error
+        }
        // byte[] val = is.readNBytes(26); // a packet is always 26 bytes. 
         byte[] val = is.readNBytes(26); 
+        // a packet is always 26 bytes.
+ 
+        return val;       
+    }
+    
+    private byte[] getAnyResponse() throws IOException
+    {
+        try 
+        {
+            Thread.sleep(10);
+        } catch (InterruptedException ex) 
+        {
+            // ignoring the error
+        }
+        byte[] val = is.readNBytes(26); // a packet is always 26 bytes. 
+        //byte[] val = is.readAllBytes(); 
         // a packet is always 26 bytes.         
         return val;
     }
@@ -145,25 +182,24 @@ public class BK8500Driver {
         // find serial port. 
          SerialPort[] seenPorts = SerialPort.getCommPorts();
         // show serial ports 
-        for (SerialPort item : seenPorts) 
-        {
-            System.out.println(item);
-        }
+        for (SerialPort item : seenPorts){System.out.println(item);}
         String portName;
         // show descriptive port names. 
-        portName = seenPorts[0].getSystemPortName(); //note this is device specific. 
-        System.out.println("port 1 = " + seenPorts[0].getDescriptivePortName() );
-        System.out.println("port 2 = " + seenPorts[1].getDescriptivePortName() );
-        System.out.println("port 3 = " + seenPorts[2].getDescriptivePortName() );
+        portName = "COM10";//seenPorts[0].getSystemPortName(); //note this is device specific. 
+        //System.out.println("port 1 = " + seenPorts[0].getDescriptivePortName() );
+        //System.out.println("port 2 = " + seenPorts[1].getDescriptivePortName() );
+        //System.out.println("port 3 = " + seenPorts[2].getDescriptivePortName() );
         // note that COM10 comes before COM2 in the array. 
         // start new driver
         BK8500Driver TestDriver = new BK8500Driver(portName);
         System.out.println("Start Remote Operation");
         TestDriver.startRemoteOperation();
         System.out.println("get Response");
-        byte[] response =  TestDriver.getResponseByte();
-                for(int item : response){
-            System.out.println(byteToString(response[item]));
+        byte[] response =  TestDriver.getAnyResponse();
+        for(int item = 0; item<response.length; item++)
+        {  
+            System.out.print(String.format("%02X ", response[item] & 0xFF));
         }
+        System.out.println();
     }   
 }
